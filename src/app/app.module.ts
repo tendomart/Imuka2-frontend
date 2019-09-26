4b66010c0a2c4f4dc966a506ac1fa85960aa0ac7
solomon m <solomonwit@gmail.com>
2019-09-26 18:22:23 +0300

    created cards components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
+import { AgentDashboardComponent } from './agent-dashboard/agent-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BodyComponent
+    BodyComponent,
+    AgentDashboardComponent
   ],
   imports: [
     BrowserModule,

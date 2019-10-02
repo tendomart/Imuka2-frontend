2ab91e32bdc56f02f3f8fec4d36a9741987b5b07
tendomart <tmkmart@gmail.com>
2019-10-02 10:51:05 +0300

    Revert "added body component"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    BodyComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,

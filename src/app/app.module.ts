d45a60197a6a349d9dfd698129da3c8162d3a7de
tendomart <tmkmart@gmail.com>
2019-10-02 14:40:02 +0300

    Cleared out corrupt files , merged Header , body and Footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-<<<<<<< HEAD
 import { HeaderComponent } from './header/header.component';
-=======
 import { BodyComponent } from './body/body.component';
->>>>>>> 74ff115ad59f6bd3dcca8f578c7e4b5ed0cc7bad
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    BodyComponent,
     FooterComponent,
-<<<<<<< HEAD
-    HeaderComponent
-=======
-    BodyComponent
->>>>>>> 74ff115ad59f6bd3dcca8f578c7e4b5ed0cc7bad
   ],
   imports: [
     BrowserModule,

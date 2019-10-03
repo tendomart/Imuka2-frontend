1a8e85d586363d783f25898090b8c5fe7f0b6534
tendomart <tmkmart@gmail.com>
2019-10-03 14:55:57 +0300

    Added Search Filter Field ,cleared index of readme.eml sccript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    SearchFilterComponent
   ],
   imports: [
     BrowserModule,

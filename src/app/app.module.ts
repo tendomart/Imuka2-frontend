e84ad72e508c4e7c9ea6459a2372b5da5ebaedef
tendomart <tmkmart@gmail.com>
2019-10-18 17:53:09 +0300

    Created Agent Add Details Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { SearchFilterComponent } from './search-filter/search-filter.component';
 //import { DashheaderComponent } from './layout/dashboard/components/dashheader/dashheader.component';
 //import { AdddealComponent } from './layout/dashboard/pages/adddeal/adddeal.component';
 import { BaselayoutComponent } from './layout/baselayout/baselayout.component';
+import { NotificationPanelComponent } from './notification-panel/notification-panel.component';
 //import { AlldealsComponent } from './layout/dashboard/pages/alldeals/alldeals.component';
 
 @NgModule({
@@ -32,6 +33,7 @@ import { BaselayoutComponent } from './layout/baselayout/baselayout.component';
    // DashheaderComponent,
    // AdddealComponent,
     BaselayoutComponent,
+    NotificationPanelComponent,
    // AlldealsComponent
   ],
   imports: [

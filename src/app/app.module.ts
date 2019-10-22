55127e2a43bc1081f92d57503174a71069f00d9d
Solomon Obwot <solobwot@gmail.com>
2019-10-22 17:05:10 +0300

    changed the dashbaord cards and layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { SearchFilterComponent } from './search-filter/search-filter.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { BodyComponent } from "./body/body.component";
+import { FooterComponent } from "./footer/footer.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSliderModule } from "@angular/material/slider";
+import { SearchFilterComponent } from "./search-filter/search-filter.component";
 //import { AgentdashboardComponent } from './layout/dashboard/agentdashboard/agentdashboard.component';
 //import { SidenavComponent } from './layout/dashboard/components/sidenav/sidenav.component';
 //import { DashanalyticsComponent } from './layout/dashboard/pages/dashanalytics/dashanalytics.component';
 //import { DashfooterComponent } from './layout/dashboard/components/dashfooter/dashfooter.component';
 //import { DashheaderComponent } from './layout/dashboard/components/dashheader/dashheader.component';
 //import { AdddealComponent } from './layout/dashboard/pages/adddeal/adddeal.component';
-import { BaselayoutComponent } from './layout/baselayout/baselayout.component';
+import { BaselayoutComponent } from "./layout/baselayout/baselayout.component";
+// import { MyBarChartComponent } from "./my-bar-chart/my-bar-chart.component";
 //import { AlldealsComponent } from './layout/dashboard/pages/alldeals/alldeals.component';
 
 @NgModule({
@@ -28,11 +29,12 @@ import { BaselayoutComponent } from './layout/baselayout/baselayout.component';
     //AgentdashboardComponent,
     //SidenavComponent,
     //DashanalyticsComponent,
-   // DashfooterComponent,
-   // DashheaderComponent,
-   // AdddealComponent,
-    BaselayoutComponent,
-   // AlldealsComponent
+    // DashfooterComponent,
+    // DashheaderComponent,
+    // AdddealComponent,
+    BaselayoutComponent
+    // MyBarChartComponent
+    // AlldealsComponent
   ],
   imports: [
     BrowserModule,
@@ -43,4 +45,4 @@ import { BaselayoutComponent } from './layout/baselayout/baselayout.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
